d6cb96026fc496a4247cf9aa504a44bf205a660e
Julian Kahnert <mail@juliankahnert.de>
2025-03-27 11:38:57 +0100

    add specific ikea light bulb

---
+++ Sources/HAImplementations/Entities/IkeaLightBulbColored.swift
@@ -0,0 +1,19 @@
+//
+//  IkeaLightBulbColored.swift
+//
+//
+//  Created by Julian Kahnert on 03.07.24.
+//
+
+import Foundation
+import HAModels
+
+public final class IkeaLightBulbColored: SwitchDevice, @unchecked Sendable {
+    public convenience init(query: EntityId.Query) {
+        self.init(switchId: EntityId(query: query, characteristic: .switcher),
+                  brightnessId: EntityId(query: query, characteristic: .brightness),
+                  // IKEA light bulbs do not expose a color temperature property, so we need to use the RGB value for setting the temperature
+                  colorTemperatureId: nil,
+                  rgbId: EntityId(query: query, characteristic: .color))
+    }
+}

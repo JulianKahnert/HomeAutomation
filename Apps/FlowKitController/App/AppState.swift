f8f2661f9c4268c6eeb37435399a2106d0b981eb
Julian Kahnert <mail@juliankahnert.de>
2025-10-03 21:03:43 +0200

    create new live activity if needed

--- Apps/FlowKitController/App/AppState.swift
+++ Apps/FlowKitController/App/AppState.swift
@@ -116,11 +116,22 @@ final class AppState {
 
             // update lastActivity
             if let activityViewState,
-                let lastActivity {
-                await lastActivity.update(.init(state: activityViewState, staleDate: nil))
-                
-                if let newestPushToken = await lastActivity.pushTokenUpdates.makeAsyncIterator().next() {
-                    await send(pushToken: newestPushToken, ofType: .liveActivityUpdate(activityName: String(describing: lastActivity.self)))
+               !activityViewState.windowStates.isEmpty {
+
+                let activity: Activity<WindowAttributes>
+                if let lastActivity {
+                    activity = lastActivity
+                    await activity.update(.init(state: activityViewState, staleDate: nil))
+                } else {
+                    activity = try Activity<WindowAttributes>.request(
+                        attributes: WindowAttributes(),
+                        content: activityViewState,
+                        pushType: .token
+                    )
+                }
+
+                if let newestPushToken = await activity.pushTokenUpdates.makeAsyncIterator().next() {
+                    await send(pushToken: newestPushToken, ofType: .liveActivityUpdate(activityName: String(describing: activity.self)))
                 }
 
             } else {

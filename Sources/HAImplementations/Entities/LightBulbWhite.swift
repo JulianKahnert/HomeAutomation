f3fdc57231f407f80178929726d429180a339476
Julian Kahnert <mail@juliankahnert.de>
2025-12-20 10:30:39 +0100

    Remove @unchecked Sendable conformance from device classes and add architecture documentation

--- Sources/HAImplementations/Entities/LightBulbWhite.swift
+++ Sources/HAImplementations/Entities/LightBulbWhite.swift
@@ -8,7 +8,7 @@
 import Foundation
 import HAModels
 
-public final class LightBulbWhite: SwitchDevice, @unchecked Sendable {
+public final class LightBulbWhite: SwitchDevice {
     public convenience init(query: EntityId.Query, skipColorTemperature: Bool = false) {
         self.init(switchId: EntityId(query: query, characteristic: .switcher),
                   brightnessId: EntityId(query: query, characteristic: .brightness),
